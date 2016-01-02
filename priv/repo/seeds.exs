ca762bd607feace9f5b77a8984883ea770c9cdad
Christopher Garrett <cmg@pzuraq.co>
2016-01-02 13:44:40 -0800

    Adds initial models

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,23 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+Tewdew.Repo.insert(%Tewdew.User{
+  id: "656efe7a-27c3-49b6-a654-a793e19327c6",
+  email: "pzuraq@gmail.com",
+  password: "lolpassword"
+})
+
+Tewdew.Repo.insert(%Tewdew.TaskList{
+  id: "3e4080df-0261-4ee2-be46-8658046ec6aa",
+  user_id: "656efe7a-27c3-49b6-a654-a793e19327c6",
+  name: "My List"
+})
+
+Tewdew.Repo.insert(%Tewdew.Task{
+  id: "dbcfe6a5-20e7-42dd-bed4-dfa3ed80f2c7",
+  task_list_id: "3e4080df-0261-4ee2-be46-8658046ec6aa",
+  name: "Finish Tewdew!",
+  ordinal: 0,
+  is_complete: false
+})
--- web/router.ex
+++ web/router.ex
@@ -5,7 +5,7 @@ defmodule Tewdew.Router do
     plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_flash
-    plug :protect_from_forgery
+    # plug :protect_from_forgery
     plug :put_secure_browser_headers
   end
 
@@ -14,13 +14,18 @@ defmodule Tewdew.Router do
   end
 
   scope "/", Tewdew do
-    pipe_through :browser # Use the default browser stack
+    pipe_through :browser
 
     get "/", PageController, :index
   end
 
   # Other scopes may use custom stacks.
-  # scope "/api", Tewdew do
-  #   pipe_through :api
-  # end
+  scope "/api", Tewdew do
+    pipe_through :api
+
+    resources "/users", UserController, except: [:new, :edit]
+
+    resources "/task-lists", TaskListController, except: [:new, :edit]
+    resources "/tasks", TaskController, except: [:new, :edit]
+  end
 end
